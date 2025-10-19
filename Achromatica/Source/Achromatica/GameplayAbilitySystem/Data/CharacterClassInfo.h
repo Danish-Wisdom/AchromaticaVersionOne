c555a3de0e18112e7ce35380b730cccdd834213b
Danish <danishmeanswisdom@gmail.com>
2025-10-19 11:38:38 -0400

    minor commit: added DataAsset

---
+++ Achromatica/Source/Achromatica/GameplayAbilitySystem/Data/CharacterClassInfo.h
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "Engine/DataAsset.h"
+#include "CharacterClassInfo.generated.h"
+
+USTRUCT()
+struct FCharacterClassDefaultInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<UGameplayEffect> DefaultAttributes;
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TSubclassOf<UGameplayAbility>> StartingAbilities;
+
+	UPROPERTY(EditDefaultsOnly)
+	TArray<TSubclassOf<UGameplayEffect>> StartingPassives;
+
+};
+
+
+/**
+ * 
+ */
+UCLASS()
+class ACHROMATICA_API UCharacterClassInfo : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	TMap<FGameplayTag, FCharacterClassDefaultInfo> ClassDefaults;
+	
+};
